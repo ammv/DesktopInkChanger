1be6415f852e9340e51f3620600c54e48188dfb5
Artem Momatov <momatovv@list.ru>
2023-10-12 12:58:12 +0300

    deleted using

--- Virus/Program.cs
+++ Virus/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Win32;
 using LnkChanger;
-
-using System.Runtime.InteropServices;
 
 namespace Virus
 {
